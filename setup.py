55dfc85e2d3e05a325ac92e9c3a2d9d6df7d6b7b
Konstantin Krastev <grizmin@gmail.com>
2020-05-15 00:41:59 +0300

    Changes in package structure

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="onion_w1_sysfs",
-    version="0.1.1",
+    name="onion-w1-sysfs",
+    version="0.1.6",
     author="Grizmin",
     author_email="grizmin@gmail.com",
     description="w1 lib using the sysfs interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/grizmin/w1-gpio-linux",
-    package_dir={'': 'python'},
+    package_dir={'': 'OneWire'},
     py_modules=["OneWire"],
-    packages=setuptools.find_packages(where='python'),
+    packages=setuptools.find_packages(where='OneWire'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
